9fe30d1ba560d9dec0ef5fb27276badea76cc42a
TR246 <m.ikezawa.12@gmail.com>
2018-12-08 06:47:34 +0900

    テーマカラーを変更

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,12 +4,12 @@ import colors from "vuetify/es5/util/colors";
 
 Vue.use(Vuetify, {
     theme: {
-        primary: "#121212", // a color that is not in the material colors palette
+        primary: "#424242", // a color that is not in the material colors palette
         accent: colors.grey.darken3,
-        secondary: colors.amber.darken3,
+        secondary: colors.green.darken3,
         info: colors.teal.lighten1,
         warning: colors.amber.base,
-        error: colors.deepOrange.accent4,
-        success: colors.green.accent3
+        error: colors.red.accent4,
+        success: colors.blue.accent3
     }
 });
