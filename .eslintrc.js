2559c70646ab84ee9d6a96f170bdadaef81b3787
TR246 <m.ikezawa.12@gmail.com>
2018-12-11 06:30:17 +0900

    ルールを戻す

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,13 +20,6 @@ module.exports = {
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-        "vue/html-indent": ["error", 4],
-        "vue/max-attributes-per-line": ["error", {
-            singleline: 3,
-            multiline: {
-                max: 1,
-                allowFirstLine: false
-            }
-        }]
+        "vue/html-indent": ["error", 4]
     }
 }
